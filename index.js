65707be75ccf4ea36683c3e966b166d7e825980f
Radik Abubjazov <abubjazov@mail.ru>
2021-12-04 21:41:06 +0300

    createServer: +http

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
+    if (req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(`
+            <h1>Form</h1>
+            <form method="post" action="/">
+                <input name="title" type="text" />
+                <button type="submit">Submit</button>
+            </form>
+        `)
+    }
+    if (req.method === 'POST') {
+        const body = []
 
-    res.write('<h1>Hello from Node.js</h1>')
-    res.write('<h2>Hello from Node.js</h2>')
-    res.write('<h3>Hello from Node.js</h3>')
-    res.end(`
-        <div style="background: red; width: 300px; height: 300px;">
-            <h1>Test 2 Node.js</h1>
-        </div>
-    `)
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1]
+
+            res.end(`
+            <h1>Your message: ${message}</h1>
+        `)
+        })
+    }
 })
 
 server.listen(3000, () => {
