0639b093767958779bc929c391e2d02dfd2988f4
Radik Abubjazov <abubjazov@mail.ru>
2021-12-05 17:34:47 +0300

    +express-handlebars

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
-const path = require('path')
+const exhbs = require('express-handlebars')
 const app = express()
 
 
+const hbs = exhbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'main.html'))
+    res.render('main')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+    res.render('about')
 })
 
 const PORT = process.env.PORT || 3000
