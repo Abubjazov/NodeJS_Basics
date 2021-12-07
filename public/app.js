d69419579337cd66b029d4768c125862a32a8658
Radik Abubjazov <abubjazov@mail.ru>
2021-12-07 22:39:49 +0300

    models/cart.js: +delete (Shopping cart)

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach(node => {
-    node.textContent = new Intl.NumberFormat('ru-RU', {
+const toCurrency = price => {
+    return new Intl.NumberFormat('ru-RU', {
         currency: 'rub',
         style: 'currency'
-    }).format(node.textContent)
+    }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+    node.textContent = toCurrency(node.textContent)
 })
 
 const $cart = document.querySelector('#cart')
@@ -15,6 +19,27 @@ if ($cart) {
             fetch('/cart/remove/' + id, {
                 method: 'delete'
             })
+                .then(res => res.json())
+                .then(cart => {
+                    if (cart.courses.length) {
+                        const html = cart.courses.map(item => {
+                            return `
+                            <tr>
+                                <td>${item.title}</td>
+                                <td>${item.count}</td>
+                                <td>
+                                    <button class="btn btn-small js-remove" data-id="${item.id}">Remove</button>
+                                </td>
+                            </tr>
+                            `
+                        }).join('')
+
+                        $cart.querySelector('tbody').innerHTML = html
+                        $cart.querySelector('.price').textContent = toCurrency(cart.price)
+                    } else {
+                        $cart.innerHTML = '<p>Shopping cart is empty</p>'
+                    }
+                })
         }
     })
 }
