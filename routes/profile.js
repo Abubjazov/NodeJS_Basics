b21ee094bdfdd02d9e8089435bcd20e9a69c8026
Radik Abubjazov <abubjazov@mail.ru>
2021-12-16 21:14:26 +0300

    proj: +middleware/fileUpload

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const router = Router()
+
+const User = require('../models/user')
 const routeProtector = require('../middleware/route-protector')
 
-router.get('/', async (req, res) => {
+router.get('/', routeProtector, async (req, res) => {
     res.render('profile', {
         title: 'Profile',
         isProfile: true,
@@ -10,8 +12,25 @@ router.get('/', async (req, res) => {
     })
 })
 
-router.post('/', async (req, res) => {
+router.post('/', routeProtector, async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id)
+        const toChange = {
+            name: req.body.name
+        }
 
+        if (req.file) {
+            toChange.avatarUrl = req.file.path
+        }
+
+        Object.assign(user, toChange)
+
+        await user.save()
+
+        res.redirect('/profile')
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 module.exports = router
